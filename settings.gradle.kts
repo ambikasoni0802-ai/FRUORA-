234fe0ac0c864416869c0dacefd449a0f49747de
ambikasoni0802-ai <ambikasoni0802@gmail.com>
2026-09-16 14:08:10 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,8 +11,7 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         maven { url = uri("https://maven.google.com") }
-        maven { url = uri("https://api.razorpay.com/maven") } // not required but harmless
-        maven { url = uri("https://jitpack.io") } // required for Razorpay checkout SDK
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
